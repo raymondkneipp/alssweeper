f9575a2cb722c97a81db26c75f73d0d8cea23e9a
Raymond Kneipp <raymond.f.kneipp@gmail.com>
2022-12-16 10:44:52 -0500

    remove banner

--- src/components/_layout/Layout/Layout.tsx
+++ src/components/_layout/Layout/Layout.tsx
@@ -1,19 +1,14 @@
 import { Banner, Cart, Footer, NavBar, Typography } from "@components";
 
 const Layout: React.FC = ({ children }) => {
-	return (
-		<>
-			<Cart />
-			<Banner>
-				<Typography variant="p">
-					We&apos;re offering 25% off all products &amp; services!
-				</Typography>
-			</Banner>
-			<NavBar />
-			<main>{children}</main>
-			<Footer />
-		</>
-	);
+  return (
+    <>
+      <Cart />
+      <NavBar />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
 };
 
 export default Layout;
